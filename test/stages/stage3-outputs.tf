f052daf0d2b0461074ad2eca8412e4e2450b6039
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-02-18 18:29:02 +0530

    updates

--- test/stages/stage3-outputs.tf
+++ test/stages/stage3-outputs.tf
@@ -8,7 +8,7 @@
          name        = module.cp4s.name
          branch      = module.cp4s.branch
          layer       = module.cp4s.layer
-         layer_dir   = module.cp4s.layer == "infrastructure" ? "1-infrastructure" : (module.gitops.layer == "services" ? "2-services" : "3-applications")
+         layer_dir   = module.cp4s.layer == "infrastructure" ? "1-infrastructure" : (module.cp4s.layer == "services" ? "2-services" : "3-applications")
          type        = module.cp4s.type
        })
      }
